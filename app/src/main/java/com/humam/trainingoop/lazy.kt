ed35b2208ebebbb6455fa23935c7027ca8ccf93b
Mammuasyir <mammuasyir0101@gmail.com>
2022-07-01 21:59:49 +0700

    modify

---
+++ app/src/main/java/com/humam/trainingoop/lazy.kt
@@ -0,0 +1,4 @@
+package com.humam.trainingoop
+
+//Lazy itu hanya bisa digunakan ketika di panggil variablenya, dan nilainya dia itu sudah pasti. makanya harus menggunakan val.
+//ibaratnya dia tidak akan berguna kalau tidak dipanggil, dan ketika dipanggil dia sudah membawa nilai tetap
